fbcfe2b3e0ec59b28ab22f27b90729b47898d15e
mkem114 <20897849+mkem114@users.noreply.github.com>
2021-05-16 14:22:31 +1000

    wrap component children in fragment
    Signed-off-by: mkem114 <20897849+mkem114@users.noreply.github.com>

--- src/components/TokenInput.js
+++ src/components/TokenInput.js
@@ -2,22 +2,24 @@ import {h} from 'preact';
 import {Input, FormControl} from '@material-ui/core';
 
 const TokenInput = (updateToken) => {
-    <FormControl>
-        <InputLabel htmlFor="api-token-input">Up API Token</InputLabel>
-        <Input id="api-token-input" aria-describedby="api-helper-text"/>
-        <FormHelperText id="api-helper-text">
-            This only stored in memory. You can get an API token from:
-            <Link target="_blank" rel="noopener noreferrer" href="https://api.up.com.au/getting_started">
-                https://api.up.com.au/getting_started
-            </Link>
-        </FormHelperText>
-    </FormControl>
-    <Button variant="contained" color="primary">
-        Paste
-    </Button>
-    <Button variant="contained" color="primary">
-        Paste
-    </Button>
+    <Fragment>
+        <FormControl>
+            <InputLabel htmlFor="api-token-input">Up API Token</InputLabel>
+            <Input id="api-token-input" aria-describedby="api-helper-text"/>
+            <FormHelperText id="api-helper-text">
+                This only stored in memory. You can get an API token from:
+                <Link target="_blank" rel="noopener noreferrer" href="https://api.up.com.au/getting_started">
+                    https://api.up.com.au/getting_started
+                </Link>
+            </FormHelperText>
+        </FormControl>
+        <Button variant="contained" color="primary">
+            Paste
+        </Button>
+        <Button variant="contained" color="primary">
+            Paste
+        </Button>
+    </Fragment>
 }
 
 export {TokenInput}
