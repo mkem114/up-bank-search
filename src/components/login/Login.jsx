677724affb47a341fee812673e031d0e705f1266
mkem114 <20897849+mkem114@users.noreply.github.com>
2021-05-19 01:45:10 +1000

    Add token input validation

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,5 +1,10 @@
 import {h, Fragment} from 'preact';
-import {Button, CircularProgress, FormControl, FormHelperText, Input, InputLabel, Link} from '@material-ui/core';
+import {
+    Button,
+    CircularProgress,
+    FormControl,
+    Link, TextField
+} from '@material-ui/core';
 import {useState} from "preact/hooks";
 
 const Login = ({updateToken, verifyingToken, tokenIsSadge}) => {
@@ -7,7 +12,7 @@ const Login = ({updateToken, verifyingToken, tokenIsSadge}) => {
     const handleTokenInputChange = (event) => updateTokenInput(event.target.value);
     const handleSubmitToken = () => updateToken(tokenInput);
     const handlePasteToken = () => {
-        const queryOpts = { name: 'clipboard-read', allowWithoutGesture: false };
+        const queryOpts = {name: 'clipboard-read', allowWithoutGesture: false};
         navigator.permissions.query(queryOpts).then(() => {
             navigator.clipboard.readText().then((clipboardString) => {
                 updateTokenInput(clipboardString)
@@ -15,27 +20,40 @@ const Login = ({updateToken, verifyingToken, tokenIsSadge}) => {
         })
     }
 
-    return <Fragment>
-        <FormControl>
-            <InputLabel htmlFor="api-token-input">Up API Token</InputLabel>
-            <Input
-                aria-describedby="token-input-helper-text"
+    const TOKEN_PREFIX = 'up:yeah:'
+    const tokenSeemsSus = !(tokenInput === '' || tokenInput.startsWith(TOKEN_PREFIX) || TOKEN_PREFIX.startsWith(tokenInput))
+    const tokenInputHelperText = !tokenSeemsSus ?
+        <>
+            This is only stored in memory. You can get an API token from:{' '}
+            <Link
+                href="https://api.up.com.au/getting_started"
+                rel="noopener noreferrer"
+                target="_blank"
+            >
+                https://api.up.com.au/getting_started
+            </Link>
+        </>
+        :
+        `This token doesn't look right; they normally start with "up:yeah:"`
+    const tokenInputLabel = !tokenSeemsSus ? 'Up API Token' : 'Error'
+
+    return <>
+        <FormControl
+            style={{
+                paddingTop: '1em'
+            }}
+        >
+            <TextField
                 disabled={verifyingToken}
+                error={tokenSeemsSus}
                 id="api-token-input"
+                helperText={tokenInputHelperText}
+                label={tokenInputLabel}
                 onChange={handleTokenInputChange}
                 type="password"
                 value={tokenInput}
+                variant="outlined"
             />
-            <FormHelperText id="token-input-helper-text">
-                This only stored in memory. You can get an API token from:
-                <Link
-                    href="https://api.up.com.au/getting_started"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                >
-                    https://api.up.com.au/getting_started
-                </Link>
-            </FormHelperText>
         </FormControl>
         <Button
             disabled={verifyingToken}
@@ -63,7 +81,7 @@ const Login = ({updateToken, verifyingToken, tokenIsSadge}) => {
                  alt="sad pepe face"/>
         </Fragment>
         }
-    </Fragment>
+    </>
 }
 
 export {Login}
